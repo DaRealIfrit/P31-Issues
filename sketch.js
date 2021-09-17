f35aab195d4f23f37c4aaab0b6eba8e1321b074d
DaRealIfrit <68937928+DaRealIfrit@users.noreply.github.com>
2021-09-17 09:56:07 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,14 +4,20 @@ const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 
+var drops = [];
+var maxDrops = 100
+
 function preload(){
     
 }
 
 function setup(){
-    createCanvas(400, 800);
+    createCanvas(500, 800);
 	rectMode(CENTER);
 
+	for(var i=0; i < maxDrops; i++){
+	drops.push(new Drop(random(0,400), random(0,400)));
+	}
 
 	engine = Engine.create();
 	world = engine.world;
@@ -22,6 +28,9 @@ function setup(){
 
 function draw(){
 	background(0);
-	drops();
+	for(var i = 0; i < maxDrops; i++){
+		drops[i].display();
+		drops[i].display();
+	}
 }   
 
